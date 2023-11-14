06508a409de94e511e792b4dafa70e4193d70b09
ahasainul <adnan31615asif@gmail.com>
2023-11-14 20:41:47 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+    theme: {
+      extend: {
+    //    colours start 
+    colors: {
+        Headersbg:'rgba(22, 12, 110, 0.5)',
+        green:'#1BBF00',
+        gry:'#4F585F',
+        overlay:'rgba(107, 99, 197, 0.7)',
+        blue:'#726E9E',
+        bag:'#F6F5FF',
+        
+    },
+    //    colours end
+    // width start 
+    maxWidth: {
+       cointainer:'1170px',
+      },
+
+    // width end
+    // font family
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif' ],
+      'web': ['Titillium Web', 'sans-serif' ],
+     
+    },
+   
+    // font family
+    // bagrawond images 
+    backgroundImage: {
+      'baground': "url('images/as.png')",
+      'bg2': "url('images/baground.png')",
+      
+    },
+    // bagrawond images 
+      
+      }
+    }
+  }
